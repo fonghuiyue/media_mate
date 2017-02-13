652bfbc2e534c812e9da57c88820d99d6cb7d239
willyb321 <wbwilliam7@gmail.com>
2017-02-13 12:31:52 +1100

    various things, look at the diff probs

--- app/util/renderErr.js
+++ app/util/renderErr.js
@@ -1,9 +1,7 @@
-// const ipc = require('electron').ipcRenderer;
 // window.onerror = function(message, filename, lineno, colno, error)
 // {
 // 	if(error != null) {
+// 		console.log(error);
 // 		ipc.send('errorInWindow', error);
-// 	} else {
-// 		ipc.send('errorInWindow', message);
 // 	}
 // };

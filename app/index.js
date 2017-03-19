ad0c60e2328a53a50ded793e2bcc105543715696
willyb321 <wbwilliam7@gmail.com>
2017-03-19 17:42:22 +1100

    menu in separate file, add getmenuitem func

--- app/index.js
+++ app/index.js
@@ -15,8 +15,8 @@ import moment from 'moment';
 import MongoClient from 'mongodb';
 import {RSSParse} from './lib/rssparse';
 import TVDB from 'node-tvdb';
-
-require('electron-debug')()
+import {init, getMenuItem} from './menu';
+require('electron-debug')();
 const tvdb = new TVDB(process.env.TVDB_KEY);
 const f = require('util').format;
 const windowStateKeeper = require('electron-window-state');
@@ -25,14 +25,12 @@ let notDL = 0;
 const user = process.env.DB_USER;
 const password = process.env.DB_PWD;
 const dburi = process.env.DB_URL;
-const authMechanism = 'DEFAULT';
 const url = f('mongodb://%s:%s@%s/media_mate?ssl=true&replicaSet=SDD-Major-shard-0&authSource=admin',
 	user, password, dburi);
 /**
  * The electron app instance
  */
 const app = electron.app;
-
 bugsnag.register('03b389d77abc2d10136d8c859391f952', {appVersion: app.getVersion(), sendCode: true});
 let win;
 /**
@@ -131,10 +129,10 @@ function createMainWindow() {
 	win.on('closed', onClosed);
 	win.on('unresponsive', function () {
 		console.log("I've frozen. Sorry about that.")
-	})
+	});
 	win.on('responsive', function () {
 		console.log("I've unfrozen. Sorry.")
-	})
+	});
 	win.webContents.on('crashed', function (e, killed) {
 		if (killed === true) {
 			console.log(e);
@@ -145,7 +143,7 @@ function createMainWindow() {
 		} else {
 			console.log(e);
 		}
-	})
+	});
 	return win;
 }
 /**
@@ -261,152 +259,7 @@ ipc.on('dldone', (event, data) => {
  */
 app.on('ready', () => {
 	mainWindow = createMainWindow();
+	init();
 	eNotify = require('electron-notify');
 	watchRSS();
 });
-const template = [
-	{
-		label: 'File',
-		submenu: [
-			{
-				label: 'Homepage',
-				click: () => {
-					win.loadURL(`file://${__dirname}/index.html`);
-				}
-			}
-		]
-	},
-	{
-		label: 'Edit',
-		submenu: [
-			{
-				role: 'cut'
-			},
-			{
-				role: 'copy'
-			},
-			{
-				role: 'paste'
-			}
-		]
-	},
-	{
-		label: 'View',
-		submenu: [
-			{
-				role: 'reload'
-			},
-			{
-				role: 'forcereload'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				role: 'togglefullscreen'
-			}
-		]
-	},
-	{
-		role: 'window',
-		submenu: [
-			{
-				role: 'minimize'
-			},
-			{
-				role: 'close'
-			}
-		]
-	},
-	{
-		role: 'help',
-		submenu: [
-			{
-				label: 'Learn More about Electron',
-				click() {
-					require('electron').shell.openExternal('http://electron.atom.io');
-				}
-			}, {
-				label: 'About',
-				click: () => openAboutWindow({
-					icon_path: path.join(__dirname, 'icon.png'), // eslint-disable-line camelcase // temp icon till i make one
-					bug_report_url: 'https://github.com/willyb321/media_mate/issues', // eslint-disable-line camelcase
-					homepage: 'https://github.com/willyb321/elite-journal'
-				})
-			}
-		]
-	}
-];
-
-if (process.platform === 'darwin') {
-	template.unshift({
-		label: app.getName(),
-		submenu: [
-			{
-				role: 'about'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				role: 'services',
-				submenu: []
-			},
-			{
-				type: 'separator'
-			},
-			{
-				role: 'hide'
-			},
-			{
-				role: 'hideothers'
-			},
-			{
-				role: 'unhide'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				role: 'quit'
-			}
-		]
-	});
-	// Edit menu.
-	template[1].submenu.push(
-		{
-			type: 'separator'
-		}
-	);
-	// Window menu.
-	template[3].submenu = [
-		{
-			label: 'Close',
-			accelerator: 'CmdOrCtrl+W',
-			role: 'close'
-		},
-		{
-			label: 'Minimize',
-			accelerator: 'CmdOrCtrl+M',
-			role: 'minimize'
-		},
-		{
-			label: 'Zoom',
-			role: 'zoom'
-		},
-		{
-			type: 'separator'
-		},
-		{
-			label: 'Bring All to Front',
-			role: 'front'
-		}
-	];
-}
-const menu = Menu.buildFromTemplate(template);
-if (process.platform !== 'darwin') {
-	Menu.setApplicationMenu(menu);
-}

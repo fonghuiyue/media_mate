88ffcb6afc61b10a9ea6a07bbc73b6bdb47dbddb
willyb321 <wbwilliam7@gmail.com>
2017-03-21 15:47:03 +1100

    using eslint with xo config, also more linting

--- app/renderjs/viewer.js
+++ app/renderjs/viewer.js
@@ -137,7 +137,7 @@ async function getImgs() {
 															if (res.filename !== '') {
 																img.children[0].src = `http://thetvdb.com/banners/${res.filename}`;
 																img.children[0].parentNode.style.display = 'inline-block';
-															} else {
+															} else if (res.filename) {
 																img.children[0].src = `file:///${__dirname}/404.png`;
 																img.children[0].parentNode.style.display = 'inline-block';
 															}

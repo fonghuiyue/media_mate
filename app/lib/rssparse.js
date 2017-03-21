fddda3268c6089baafada1cf8c87c7e92a1c0193
willyb321 <wbwilliam7@gmail.com>
2017-03-21 15:36:52 +1100

    change  ? : to if else

--- app/lib/rssparse.js
+++ app/lib/rssparse.js
@@ -6,8 +6,11 @@ const isRenderer = require('is-electron-renderer');
 
 let version;
 // Make sure that version can be got from both render and main process
-isRenderer ? version = require('electron').remote.app.getVersion() : // eslint-disable-line no-unused-expressions
-version = require('electron').app.getVersion();
+if (isRenderer) {
+	version = require('electron').remote.app.getVersion();
+} else {
+	version = require('electron').app.getVersion();
+}
 
 bugsnag.register('03b389d77abc2d10136d8c859391f952', {appVersion: version, sendCode: true});
 

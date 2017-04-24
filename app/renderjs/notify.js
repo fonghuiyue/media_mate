59d535eb2f239f7761dc8f519ff03c84a44bec4b
Willyb321 <wbwilliam7@gmail.com>
2017-04-24 18:03:51 +1000

    catch cancel in onboard

--- app/renderjs/notify.js
+++ app/renderjs/notify.js
@@ -17,6 +17,10 @@ function firstrun() {
 		confirmButtonText: 'Yes!'
 	}).then(function () {
 		require('electron').remote.getCurrentWindow().loadURL(`file://${__dirname}/onboard.html`)
-	})
+	}).catch(err => {
+		if (err !== 'cancel') {
+			throw err;
+		}
+	});
 
 }
